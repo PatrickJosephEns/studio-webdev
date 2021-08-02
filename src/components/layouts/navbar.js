9af4ff732d7ca95a0de8b73fdc990be3d0eb4e2a
PatrickJosephEns <patens6@gmail.com>
2021-08-02 13:31:22 +1200

    added login button to navbar

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -9,7 +9,10 @@ function NavBar()  {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link" href="http://localhost:3000/">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Another Link <span class="sr-only"></span></a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -17,10 +20,14 @@ function NavBar()  {
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="#">Data</a>
-            <a class="nav-link" href="#">About</a>
+            <a class="dropdown-item" href="#">About</a>
           </div>
+
         </li>
       </ul>
+      <form class="form-inline my-2 my-lg-0">
+      <a class="btn btn-outline-info my-2 my-sm-0" type="submit" href="/login">Sign In</a>
+    </form>
     </div>
   </nav>
     );
