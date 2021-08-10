7ec6bdb9d17066d3f32b205d8f2c10666481104a
KieranBrett <brett.kieran@hotmail.com>
2021-08-11 09:40:48 +1200

    Updated the model

--- src/components/pages/Home/home.js
+++ src/components/pages/Home/home.js
@@ -3,12 +3,15 @@ import Display_model from './display_model';
 
 function Home() {
   return (
-    <div id="home">
-      <Display_model />
-      <div class="title">
-        <p class="header">The Mall</p>
-        <p class="paragraph">Experience the future of shopping</p>
+    <div id="content">
+      <div id="home">
+        <Display_model />
+        <div class="title">
+          <p class="header">The Mall</p>
+          <p class="paragraph">Experience the future of shopping</p>
+        </div>
       </div>
+      
     </div>
   );
 }
