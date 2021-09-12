962ef558132ab637146f735ec105a8a1ffa95d36
Kieran <brett.kieran@hotmail.com>
2021-09-13 11:56:27 +1200

    can now add items with a category and a description

--- src/components/store/item-crud/CreateItemForm.js
+++ src/components/store/item-crud/CreateItemForm.js
@@ -1,44 +1,87 @@
-import React from 'react';
+import React from "react";
 
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
+// Data Inputs
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import TextField from "@material-ui/core/TextField";
+
+// Layout
+import Grid from "@material-ui/core/Grid";
 
 class AddItemForm extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            item: ''
-        }
+  constructor() {
+    super();
+    this.state = {
+      item: "",
+      desc: "",
+      category: "",
+    };
 
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(e) {
-        this.setState({
-            item: e.target.value
-        })
-    }
+  handleChange(e) {
+    console.log(e.target.name);
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
 
-    handleSubmit(e) {
-        this.props.db.collection("stores").doc(this.props.store_id).collection("items").add({
-            text: this.state.item
-        })
+  handleSubmit(e) {
+    this.props.db
+      .collection("stores")
+      .doc(this.props.store_id)
+      .collection("items")
+      .add({
+        text: this.state.item,
+        desc: this.state.desc,
+        category: this.state.category,
+      });
 
-        this.setState({
-            item: ''
-        })
+    this.setState({
+      item: "",
+      desc: "",
+      category: "",
+    });
 
-        e.preventDefault();
-    }
+    e.preventDefault();
+  }
 
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Grid container direction="column" justifyContent="space-between" alignItems="flex-start" spacing={1}>
+        <TextField name="item" label="Item Name" onChange={this.handleChange} />
+        <TextField name="desc" label="Item Description" onChange={this.handleChange} />
 
-    render() {
-        return <form onSubmit={(this.handleSubmit)}>
-            <input type="text" value={this.state.item} placeholder="Item Name" onChange={(this.handleChange)}></input>
-            <Button color="primary" size="small" variant="contained" type="submit">Add</Button>
-        </form>
-    }
+        <FormControl>
+          <InputLabel>Category</InputLabel>
+          <Select
+            native
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+            input={<Input id="demo-dialog-native" />}
+          >
+            <option aria-label="None" value="" />
+            <option value={"Tech"}>Tech</option>
+            <option value={"Clothes"}>Clothes</option>
+            <option value={"Food"}>Food</option>
+          </Select>
+        </FormControl>
+
+        <Button color="primary" size="small" variant="contained" type="submit">
+          Add
+        </Button>
+        </Grid>
+      </form>
+    );
+  }
 }
 
-export default AddItemForm
+export default AddItemForm;
